1cda052a0977534014260bd54c04678f3b89e368
arsixxy <72648010+arsixxy@users.noreply.github.com>
2022-03-03 21:21:07 +0400

    Delete JSgame directory

--- JSgame/endMenu.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('showEndMenu', () => {
-setTimeout(() =>{
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    var endMenu = document.getElementById('endMenu')
-    endMenu.style= "display: block"    
-}, 500)
-})
-
